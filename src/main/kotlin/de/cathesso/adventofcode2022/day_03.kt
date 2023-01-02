ed11e8ea8e77ccace5bcc56539d224d7d41e66ad
Fabian Fromme <harrofabian.fromme@otto.de>
2023-01-02 22:53:32 +0100

    Add AdventOfCode Day 03_2

--- src/main/kotlin/de/cathesso/adventofcode2022/day_03.kt
+++ src/main/kotlin/de/cathesso/adventofcode2022/day_03.kt
@@ -6,18 +6,24 @@ class Backpack (items: String){
     val allItems = items
     val compartmentA: String = items.substring(0, items.length /2)
     val compartmentB: String = items.substring(items.length/2)
-    val itemsInBothCompartments: MutableList<Char> = backpackCompartmentComparer(this)
+    val duplicates: MutableList<Char> = itemListComparer(compartmentA, compartmentB)
 }
 
 fun main(){
     val backpackListRaw = loadInputFromTextAndSaveAsList("day_03_input.txt")
     val backpackList = backpackMapper(backpackListRaw)
+    println("Task one: Single backpacks")
     for (backpack in backpackList){
         println("A: " + backpack.compartmentA)
         println("B: " + backpack.compartmentB)
-        println("Item(s)" + backpack.itemsInBothCompartments)
+        println("Item(s)" + backpack.duplicates)
     }
-    println("Total Priority Count: " + backpackItemInBothCompartmentsPriorityCountUp(backpackList))
+    println("Total Priority Count: " + backpackDuplicatesPriorityCountUp(backpackList))
+    println('*'.toString().repeat(42))
+    println("Task two: Badges of three backpacks: ")
+    val badges = compareTripleBackpacks(backpackList)
+    println(badges)
+    println("Total Priority Count: " + priorityCountUp(badges))
 }
 
 fun backpackMapper(backpackListRaw: List<String>): MutableList<Backpack>{
@@ -28,27 +34,40 @@ fun backpackMapper(backpackListRaw: List<String>): MutableList<Backpack>{
     return backpackList
 }
 
-fun backpackCompartmentComparer(backpack: Backpack): MutableList<Char>{
-    val itemsInBothCompartments :MutableList<Char> = mutableListOf()
-    for (item in backpack.compartmentA){
-        if (backpack.compartmentB.contains(item) && !itemsInBothCompartments.contains(item)){
-            itemsInBothCompartments.add(item)
+fun itemListComparer(itemListOne :String, itemListTwo :String): MutableList<Char>{
+    val itemsInBothLists :MutableList<Char> = mutableListOf()
+    for (item in itemListOne){
+        if (itemListTwo.contains(item) && !itemsInBothLists.contains(item)){
+            itemsInBothLists.add(item)
         }
     }
-    return itemsInBothCompartments
+    return itemsInBothLists
 }
 
-
-fun backpackItemInBothCompartmentsPriorityCountUp(backpackList :MutableList<Backpack>) :Int{
+fun backpackDuplicatesPriorityCountUp(backpackList :MutableList<Backpack>) :Int{
     var priorityCountUp :Int = 0
     for (backpack in backpackList){
-        for (item in backpack.itemsInBothCompartments){
-            priorityCountUp += (alphabetNumeric.indexOf(item) + 1)
-        }
+        priorityCountUp += priorityCountUp(backpack.duplicates)
     }
     return priorityCountUp
 }
 
+fun priorityCountUp (itemList :MutableList<Char>): Int{
+    var priorityCountUp :Int = 0
+    for (item in itemList){
+        priorityCountUp += (alphabetNumeric.indexOf(item) + 1)
+    }
+    return priorityCountUp
+}
 
-//Jeder Rucksack hat zwei Behälter
-//Alle Items von einem Typ sollen in jeweils einen Behälter
+fun compareTripleBackpacks(backpackList: MutableList<Backpack>) :MutableList<Char>{
+    val badges :MutableList<Char> = mutableListOf()
+    for (i in 0..backpackList.lastIndex step 3){
+        val comparisonOne = itemListComparer(backpackList[i].allItems, backpackList[i+1].allItems).toString()
+        val comparisonTwo = itemListComparer(comparisonOne, backpackList[i+2].allItems)
+        for (item in comparisonTwo){
+            badges.add(item)
+        }
+    }
+    return badges
+}
